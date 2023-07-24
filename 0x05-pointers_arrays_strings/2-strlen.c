d82ffa0955386209d8e8db5e7b9c33af1248fd63
MandileN <mandilenosipho2@gmail.com>
2023-07-24 13:36:36 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,20 +1,16 @@
 #include "main.h"
-
 /**
  * _strlen - returns the length of a string
- * @s: string
- * Ruturn: length
+ * @s: string to be counted
+ * Ruturn: length of the string
  */
-
 int _strlen(char *s)
 {
 	int longi = 0;
-
 	while (*s != '\0')
 	{
 		longi++;
-		s++;
+		s++
 	}
-
 	return (longi);
 }
