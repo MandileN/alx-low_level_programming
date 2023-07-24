9fa9248832719ef9b3c16ffa3f37fa49975a11d0
MandileN <mandilenosipho2@gmail.com>
2023-07-24 10:51:00 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,12 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN _H
 
 #include <stdio.h>
 #include <unistd.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
-void swap_int(int *a, int *b)
+void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
@@ -14,6 +14,6 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 
 #endif
