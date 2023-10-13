f9dc60299edecddb7b1122c3ed13cfa6a3167c4b
Nosipho Mandile <136008388+MandileN@users.noreply.github.com>
2023-10-13 10:11:00 +0000

    0x15-file_io

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,8 +6,7 @@
  * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
-{
-        int i;
+{ int i;
         unsigned int dec_val = 0;
         if (!b)
                 return (0);
